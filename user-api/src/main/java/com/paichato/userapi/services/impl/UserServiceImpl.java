b07080cf401b9ca694d1c1da11a6080bfc6878fc
paichato <tsenane14@gmail.com>
2023-10-24 16:57:40 +0300

    feat: add logs

--- user-api/src/main/java/com/paichato/userapi/services/impl/UserServiceImpl.java
+++ user-api/src/main/java/com/paichato/userapi/services/impl/UserServiceImpl.java
@@ -5,20 +5,26 @@ import com.paichato.userapi.repositories.UserRepository;
 import com.paichato.userapi.services.UserService;
 import com.paichato.userapi.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository repository;
+    private final Environment env;
 
     @Override
     public User findById(Long id) {
+        log.info("USER_SERVICE ::: GET REQUEST on " + env.getProperty("local.server.port")+ "port");
+
         return repository.findById(id).orElseThrow(()-> new ObjectNotFoundException("Object Not found"));
     }
 
